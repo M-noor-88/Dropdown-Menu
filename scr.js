e265768da22980f5fe15e43b00332ec278d6ab8f
M-noor-88 <mohtakaleh21@gmail.com>
2022-04-27 18:34:52 +0300

    Upload Files 01

---
+++ scr.js
@@ -0,0 +1,8 @@
+
+
+let navigation = document.querySelector('.navigation');
+let menuToggle = document.querySelector('.menuToggel');
+
+menuToggle.onclick = function () {
+    navigation.classList.toggle('active');
+}
